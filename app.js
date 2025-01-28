82878338d13068f1841e50fe7b6bb4e8dca8cb40
saikrishnakolluri <80934960+saikrishnakolluri@users.noreply.github.com>
2025-01-28 07:08:46 +0000

    Completed the lab

--- app.js
+++ app.js
@@ -1,2 +1,123 @@
-/* add your code here */
+document.addEventListener('DOMContentLoaded', () => {
+    // function will go here
+  
+    const stocksData = JSON.parse(stockContent);
+    const userData = JSON.parse(userContent);
 
+
+function generateUserList(users, stocks) {
+    // get the list element and for each user create a list item and append it to the list
+    const userList = document.querySelector('.user-list');
+    userList.innerHTML = ''; // clear out the list from previous render
+    users.map(({ user, id }) => {
+      const listItem = document.createElement('li');
+      listItem.innerText = user.lastname + ', ' + user.firstname;
+      listItem.setAttribute('id', id);
+      userList.appendChild(listItem);
+    });
+
+    // register the event listener on the list
+    userList.addEventListener('click', (event) => handleUserListClick(event, users, stocks));
+  }
+
+  function handleUserListClick(event, users, stocks) {
+    // get the user id from the list item
+    const userId = event.target.id;
+    // find the user in the userData array
+    const user = users.find(user => user.id == userId);
+    // populate the form with the user's data
+    populateForm(user);
+
+    // render the portfolio items for the user
+    renderPortfolio(user, stocks);
+  }
+
+  function populateForm(data) {
+    const { user, id } = data;
+    document.querySelector('#userID').value = id;
+    document.querySelector('#firstname').value = user.firstname;
+    document.querySelector('#lastname').value = user.lastname;
+    document.querySelector('#address').value = user.address;
+    document.querySelector('#city').value = user.city;
+    document.querySelector('#email').value = user.email;
+  }
+
+  function renderPortfolio(user, stocks) {
+    // get the user's stock data
+    const { portfolio } = user;
+    // get the portfolio list element
+    const portfolioDetails = document.querySelector('.portfolio-list');
+    portfolioDetails.innerHTML = ''; // clear the list from previous render
+    portfolio.map(({ symbol, owned }) => {
+      // create a list item and append it to the list
+      const symbolEl = document.createElement('p');
+      const sharesEl = document.createElement('p');
+      const actionEl = document.createElement('button');
+      symbolEl.innerText = symbol;
+      sharesEl.innerText = owned;
+      actionEl.innerText = 'View';
+      actionEl.setAttribute('id', symbol);
+      portfolioDetails.appendChild(symbolEl);
+      portfolioDetails.appendChild(sharesEl);
+      portfolioDetails.appendChild(actionEl);
+    });
+  }
+
+  function viewStock(symbol, stocks) {
+    // begin by hiding the stock area until a stock is viewed
+    const stockArea = document.querySelector('.stock-form');
+    if (stockArea) {
+      // find the stock object for this symbol
+      const stock = stocks.find(function (s) { return s.symbol == symbol; });
+
+      document.querySelector('#stockName').textContent = stock.name;
+      document.querySelector('#stockSector').textContent = stock.sector;
+      document.querySelector('#stockIndustry').textContent = stock.subIndustry;
+      document.querySelector('#stockAddress').textContent = stock.address;
+
+      document.querySelector('#logo').src = `logos/${symbol}.svg`;
+    }
+  }
+
+  generateUserList(userData, stocksData);
+
+  const saveButton = document.querySelector('#save-button');
+  // Register the event listener on the save button
+  saveButton.addEventListener('click', (event) => {
+    // we don't want the form to submit (since we will lose form state)
+    event.preventDefault();
+
+    // find the user object in our data
+    const id = document.querySelector('#userID').value;
+
+    for (let i = 0; i < userData.length; i++) {
+      // found relevant user, so update object at this index and redisplay
+      if (userData[i].id == id) {
+        userData[i].user.firstname = document.querySelector('#firstname').value;
+        userData[i].user.lastname = document.querySelector('#lastname').value;
+        userData[i].user.address = document.querySelector('#address').value;
+        userData[i].user.city = document.querySelector('#city').value;
+        userData[i].user.email = document.querySelector('#email').value;
+
+        generateUserList(userData, stocksData);
+        break; // Exit the loop after updating the user
+      }
+    }
+  });
+
+  const deleteButton = document.querySelector('#delete-button');
+  // Register the event listener on the delete button
+  deleteButton.addEventListener('click', (event) => {
+    // we don't want the form to submit (since we will lose form state)
+    event.preventDefault();
+
+    // find the index of the user in the data array
+    const userId = document.querySelector('#userID').value;
+    const userIndex = userData.findIndex(user => user.id == userId);
+    // remove the user from the array
+    userData.splice(userIndex, 1);
+    // render the user list
+    generateUserList(userData, stocksData);
+  });
+  
+});
